f104a94eaee0eba2db35970cff11e56c8ecfed5a
Aleksandr <pulatov.alexander@gmail.com>
2025-06-11 18:36:00 +0300

    Add main screen

--- abz.agency/Modules/MainView/MainView.swift
+++ abz.agency/Modules/MainView/MainView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            NavigationTitleView(title: "Working with GET request")
+            
+            Spacer()
+        }
     }
 }
 
 #Preview {
     MainView()
+}
+
+struct NavigationTitleView: View {
+    let title: String
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(FontFamily.NunitoSans.regular.swiftUIFont(size: 20))
+                .foregroundStyle(Asset.Colors.darkBlack.swiftUIColor)
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: UIScreen.main.bounds.height * 0.0875)
+        .background(Asset.Colors.yellow.swiftUIColor)
+        .padding(.top, 0.5)
+    }
 }
