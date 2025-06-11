023b6d5ce5a81f497b51a3b52c1367460d256e47
Aleksandr <pulatov.alexander@gmail.com>
2025-06-11 17:04:58 +0300

    Setup project

---
+++ abz.agency/abz_agencyApp.swift
@@ -0,0 +1,18 @@
+//
+//  abz_agencyApp.swift
+//  abz.agency
+//
+//  Created by Alexander on 11.06.25.
+//
+
+import SwiftUI
+
+@main
+struct abz_agencyApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+         
+        }
+    }
+}
